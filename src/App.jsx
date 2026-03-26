0fc2dce38b9ccb6851730da34d0e6642cc66cd42
muxammaz0123456789-hue <muxammaz.0123456789@gmail.com>
2026-03-26 09:48:57 +0500

    Section 2v 3 tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import Header from './Components/Header/header'
+import SectionThree from './Components/SectionThree/section'
+import SectionFour from './Components/SectionFour/section'
 
 function App() {
 
   return (
     <>
       <Header />
+      <SectionThree/>
+      <SectionFour/>
     </>
   )
 }
---
+++ src/Components/SectionFour/section.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import './section.css';
+import rasmcha from '../../assets//maymun.png';
+
+
+function SectionFour() {
+  return (
+    <section className="hero-section container">
+      
+      <div className="hero-illustration">
+        <img src={rasmcha} alt="" />
+      </div>
+
+      <div className="hero-content">
+        <span className="subtitle">Portfolio Service</span>
+        <h1 className="main-title">We help design a ecommerce business shop</h1>
+        <p className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed
+          euismod quis sit gravida pharetra sit. Cursus vel enim pretium
+          vulputate semper sem elit magna neque. Aliquet adipisc
+        </p>
+        <div className="button-group">
+          <a href="#" className="see-live">
+            <div className="play-icon">▶</div>
+            See live
+          </a>
+          <a href="#" className="case-study">Case study</a>
+        </div>
+      </div>
+
+
+    </section>
+  );
+}
+
+export default SectionFour;
